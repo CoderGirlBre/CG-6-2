fd4b1773b046682b6d4b06e563321742fbd4e7eb
CoderGirlBre <41300229+CoderGirlBre@users.noreply.github.com>
2018-08-19 00:48:22 -0500

    Add files via upload

--- Uppercase/Program.cs
+++ Uppercase/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Uppercase
 {
@@ -6,7 +7,18 @@ namespace Uppercase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //This passes whatever the user entered into the literal string.
+            //Then, it is assigned to a new string called result.
+            Console.WriteLine("Type a sentence. {0} ", "");
+            string result = Console.ReadLine();
+
+            StringBuilder mystring = new StringBuilder();
+            //The stringbuilder variable concatenates the result with to upper, making what the user entered all uppercase.
+            mystring.Append(result.ToUpper());
+
+            //This what the user entered in all uppercase letters to the console.
+            Console.WriteLine(mystring);
+            Console.ReadLine();
         }
     }
 }
